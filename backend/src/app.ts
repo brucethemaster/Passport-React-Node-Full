721847028fd50f1f986749bd690317a188580512
Bruce Qin <qinfeng21@hotmail.com>
2022-06-04 11:13:24 -0700

    restructre app and server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,19 @@
 import express from 'express';
-
+import connect from './utils/connect';
 import routes from './routes';
 import cors from 'cors';
 
 import passport from 'passport';
 import passportStrategy from './config/passport';
 
-const app = async () => {
+const app = async (dbUrl) => {
 	const app = express();
 	app.use(cors());
 	passportStrategy(passport);
 	app.use(passport.initialize());
 	app.use(express.json());
 	routes(app);
+	await connect(dbUrl);
 	return app;
 };
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,9 +4,8 @@ import logger from './utils/logger';
 const port = (process.env.PORT || 3000) as number;
 const dbUrl = (process.env.MONGODB_URL || '') as string;
 
-app().then((app) => {
+app(dbUrl).then((app) => {
 	app.listen(port, async () => {
 		logger.info(`Express server is running on port:${port}`);
-		await connect(dbUrl);
 	});
 });
