9558129e8782e06b3285c610037fc8de567d8659
Bruce Qin <qinfeng21@hotmail.com>
2022-06-04 00:00:24 -0700

    update user schema test

--- backend/test/unit/schema/index.test.ts
+++ backend/test/unit/schema/index.test.ts
@@ -7,9 +7,8 @@ import {
 import validateSchema from '../../../src/middleware/validateMiddleware';
 import { NextFunction, Request, Response } from 'express';
 import mocks from 'node-mocks-http';
+import payload from './payloads';
 describe('schema validation', () => {
-	const loginGoodPayload = { email: 'test1@test.com', password: '123321' };
-	const loginBadPayload = { email: 'test1test.com' };
 	const mockNext = () => {
 		const next = jest.fn();
 		return next;
@@ -28,15 +27,62 @@ describe('schema validation', () => {
 
 	it('should pass the login if payload is valid', () => {
 		const validateSchemaTester = validateSchema(loginUserSchema);
-		req.body = loginGoodPayload;
-		validateSchemaTester(req, res, next);
 
+		req.body = payload.loginGoodPayload;
+		validateSchemaTester(req, res, next);
 		expect(next).toHaveBeenCalled;
 	});
 
-	it('should fail the login if payload is inValid', () => {
+	it('should fail the login if payload is invalid', () => {
 		const validateSchemaTester = validateSchema(loginUserSchema);
-		req.body = loginBadPayload;
+
+		req.body = payload.loginBadPayload;
+		validateSchemaTester(req, res, next);
+		expect(res.statusCode).toEqual(400);
+	});
+
+	it('should pass the sing up if payload is valid', () => {
+		const validateSchemaTester = validateSchema(signupUserSchema);
+
+		req.body = payload.sinupGoodPayload;
+		validateSchemaTester(req, res, next);
+		expect(next).toHaveBeenCalled;
+	});
+
+	it('should fail the sing up if payload is invalid', () => {
+		const validateSchemaTester = validateSchema(createUserSchema);
+
+		req.body = payload.sinupBadPayload;
+		validateSchemaTester(req, res, next);
+		expect(res.statusCode).toEqual(400);
+	});
+	it('should pass the create user if payload is valid', () => {
+		const validateSchemaTester = validateSchema(createUserSchema);
+
+		req.body = payload.createUserGoodPayload;
+		validateSchemaTester(req, res, next);
+		expect(next).toHaveBeenCalled;
+	});
+
+	it('should fail the create user if payload is invalid', () => {
+		const validateSchemaTester = validateSchema(updateUserSchema);
+
+		req.body = payload.createUserBadPayload;
+		validateSchemaTester(req, res, next);
+		expect(res.statusCode).toEqual(400);
+	});
+	it('should pass the update user if payload is valid', () => {
+		const validateSchemaTester = validateSchema(updateUserSchema);
+
+		req.body = payload.updateUserGoodPayload;
+		validateSchemaTester(req, res, next);
+		expect(next).toHaveBeenCalled;
+	});
+
+	it('should fail the update user if payload is invalid', () => {
+		const validateSchemaTester = validateSchema(signupUserSchema);
+
+		req.body = payload.updateUserBadPayload;
 		validateSchemaTester(req, res, next);
 		expect(res.statusCode).toEqual(400);
 	});
