d4e2d0231b9fcea37eb4423ea46a8ebc41e1b7a5
Bruce Qin <qinfeng21@hotmail.com>
2022-06-03 21:38:41 -0700

    adding index.ts for all schemas

---
+++ backend/src/schema/index.ts
@@ -0,0 +1,4 @@
+import { loginUserSchema } from './login.schema';
+import { signupUserSchema, updateUserSchema, createUserSchema } from './user.schema';
+
+export { loginUserSchema, signupUserSchema, createUserSchema, updateUserSchema };
